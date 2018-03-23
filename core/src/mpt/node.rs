b0dcc29f45ab4d5f1fd2d4920ef414962f3b7e8c
Edward Tan <etan@fulgent.com>
2018-03-23 10:22:14 -0700

    Updated.

--- core/src/mpt/node.rs
+++ core/src/mpt/node.rs
@@ -4,7 +4,7 @@ use std::iter::Iterator;
 use self::common::hash::*;
 use std::fmt::{ Debug, Formatter };
 
-static INDICES: [String; 17] = ["0".to_string(), "1".to_string(), "2".to_string(), "3".to_string(), "4".to_string(), "5".to_string(), "6".to_string(), "7".to_string(), "8".to_string(), "9".to_string(), "a".to_string(), "b".to_string(), "c".to_string(), "d".to_string(), "e".to_string(), "f".to_string(), "[17]".to_string()];
+static INDICES: [&'static str; 17] = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f", "[17]"];
 
 pub type TrieKey = Hash;
 
@@ -14,13 +14,13 @@ pub type ValueNode = Vec<u8>;
 
 pub struct BranchNode<'a> {
     router: [Option<&'a NodeOp>; 17],
-    flags: NodeFlag
+    flags: NodeFlag<'a>
 }
 
 #[derive(Default)]
-pub struct NodeFlag {
+pub struct NodeFlag<'a> {
     /// cached hash of the node (may be nil)
-    pub hash: Option<ExtensionNode>,
+    pub hash: Option<&'a ExtensionNode>,
     /// cache generation counter
     pub gen: u16,
     /// whether the node has changes that must be written to the database
@@ -28,7 +28,7 @@ pub struct NodeFlag {
 
 }
 
-impl NodeFlag {
+impl<'a> NodeFlag<'a> {
     pub fn can_unload(&self, cachegen: u16, cachelimit: u16) -> bool {
         !self.dirty && cachegen - self.gen >= cachelimit
     }
@@ -37,7 +37,7 @@ impl NodeFlag {
 pub trait NodeOp {
     fn print(&self, index: &String) -> String;
 
-    fn cache(&self) -> (Option<ExtensionNode>, bool);
+    fn cache(&self) -> (Option<& ExtensionNode>, bool);
 
     fn can_unload(&self, cachegen: u16, cachelimit: u16) -> bool;
 }
@@ -48,14 +48,14 @@ impl<'a> NodeOp for BranchNode<'a> {
         buff = buff + format!("[\n{}\t", index.as_str()).as_str();
         for (i, elem) in self.router.iter().enumerate() {
             match *elem {
-                Some(n) => buff = buff + format!("{}: {}", INDICES[i].as_str(), (*n).print(&(*index + "\t")).as_str()).as_str(),
-                None => buff = buff + format!("{}: <nil> ", INDICES[i].as_str()).as_str(),
+                Some(n) => buff = buff + format!("{}: {}", INDICES[i], (*n).print(&(index.clone() + "\t")).as_str()).as_str(),
+                None => buff = buff + format!("{}: <nil> ", INDICES[i]).as_str(),
             };
         }
         buff + format!("\n{}] ", index).as_str()
     }
 
-    fn cache(&self) -> (Option<ExtensionNode>, bool) {
+    fn cache(&self) -> (Option<& ExtensionNode>, bool) {
         (self.flags.hash, self.flags.dirty)
     }
 
