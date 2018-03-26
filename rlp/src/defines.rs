d52d3813c1e196ed9c205a9b6b842d52646b991c
edwardtanshuo <tanshuo198956@gmail.com>
2018-03-25 21:51:51 -0800

    RLP

--- rlp/src/defines.rs
+++ rlp/src/defines.rs
@@ -8,3 +8,5 @@ pub const SINGLE_BYTE_MAX_VALUE: u8 = 0x7fu8;
 
 pub const SHORT_STRING_PREFIX_BASE: u8 = 0x80u8;
 pub const SHORT_STRING_MAX_LEN: usize = 55usize;
+
+pub const LONG_STRING_PREFIX_BASE: u8 = 0xb7;
--- rlp/src/encoder.rs
+++ rlp/src/encoder.rs
@@ -1,14 +1,34 @@
 extern crate bytebuffer;
 
-use self::bytebuffer::ByteBuffer;
+use self::bytebuffer::*;
 use defines::*;
+use std::io::{Read, Write, Result};
+use std::mem::*;
+
+macro_rules! total_bytes {
+    ($e:expr) => {
+        if ($e << 8) == 0 { 1u8 }
+        else if ($e << 16) == 0 { 2u8 }
+        else if ($e << 24) == 0 { 3u8 }
+        else if ($e << 32) == 0 { 4u8 }
+        else if ($e << 40) == 0 { 5u8 }
+        else if ($e << 48) == 0 { 6u8 }
+        else { 7u8 }
+    };
+}
 
 struct Encoder {
     buffer: ByteBuffer
 }
 
 impl Encoder {
-    fn new() -> Self {
+    fn new(size: usize) -> Self {
+        let mut buffer = ByteBuffer::new();
+        buffer.resize(size);
+        Encoder { buffer: buffer }
+    }
+
+    fn new_with_size() -> Self {
         let mut buffer = ByteBuffer::new();
         buffer.resize(ENCODER_BUFFER_SIZE);
         Encoder { buffer: buffer }
@@ -16,32 +36,56 @@ impl Encoder {
 }
 
 impl Encoder {
-    fn encode_byte(&self, input: &u8) {
-        if *input > SINGLE_BYTE_MAX_VALUE {
+    fn encode_byte_len(&self, input: u8) -> usize {
+        return 1;
+    }
+
+    fn encode_byte(&mut self, input: u8) {
+        if input > SINGLE_BYTE_MAX_VALUE {
             panic!("Byte value is greater than 0x7f.");
         } else {
             self.buffer.write_u8(input);
         }
     }
 
-    fn encode_short_str(&self, input: &str) {
+    fn encode_short_str_len(& self, input: &str) -> usize {
+        return 1 + input.len();
+    }
+
+    fn encode_short_str(&mut self, input: &str) {
         if input.len() > SHORT_STRING_MAX_LEN {
             panic!("String length out of range 0-55.");
         } else {
             let prefix: u8 = SHORT_STRING_PREFIX_BASE + input.len() as u8;
-            self.buffer.write(prefix);
+            self.buffer.write(&[prefix]);
             self.buffer.write(input.as_bytes());
         }
     }
 
-    fn encode_long_str(&self, input: &str) {
-        if input.len() > SHORT_STRING_MAX_LEN {
-            panic!("String length out of range 0-55.");
+    fn encode_long_str_len(& self, input: &str) -> usize {
+        let l = input.len() as u64;
+        let l_total_byte = total_bytes!(l);
+        return 1 + l_total_byte + input.len();
+    }
+
+    fn encode_long_str(&mut self, input: &str) {
+        if input.len() <= SHORT_STRING_MAX_LEN {
+            panic!("String length is no enough for encoding.");
         } else {
-            let prefix: u8 = SHORT_STRING_PREFIX_BASE + input.len() as u8;
-            self.buffer.write(prefix);
+            let l = input.len() as u64;
+            let l_total_byte = total_bytes!(l);
+
+            let prefix: u8 = LONG_STRING_PREFIX_BASE + l_total_byte;
+
+            self.buffer.write(&[prefix]);
+            let len_bytes: [u8; 8] = unsafe { transmute(l.to_be()) };
+            for i in 0..l_total_byte {
+                self.buffer.write_u8(len_bytes[i as usize]);
+            }
             self.buffer.write(input.as_bytes());
         }
     }
+
+
 }
 
