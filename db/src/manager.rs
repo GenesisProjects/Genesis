96d72064c87a93edca3e08a68515560aaf5b5a91
Edward Tan <etan@fulgent.com>
2018-03-30 12:00:51 -0700

    Mock DB

--- db/src/manager.rs
+++ db/src/manager.rs
@@ -35,8 +35,17 @@ pub struct DBManager {
     config: &'static mut DBConfig
 }
 
-// TODO:
+#[cfg(mock)]
 lazy_static! {
+    pub static ref SHARED_MANAGER: Mutex<DBManager> = {
+        static mut conf: DBConfig = DBConfig {};
+        unsafe { Mutex::new(DBManager{ config: &mut conf }) }
+    };
+}
+
+#[cfg(not(mock))]
+lazy_static! {
+    //TODO:
     pub static ref SHARED_MANAGER: Mutex<DBManager> = {
         static mut conf: DBConfig = DBConfig {};
         unsafe { Mutex::new(DBManager{ config: &mut conf }) }
@@ -56,6 +65,29 @@ pub trait DBManagerOP {
     fn show_status(&self) -> Result<DBStatus, DBError>;
 }
 
+#[cfg(mock)]
+impl DBManagerOP for DBManager {
+    fn connect(&self,config: & DBConfig) -> Result<(&'static DBContext, DBResult), DBError> {
+        Err(DBError::DBConnectError { msg: "Unknown Err" })
+    }
+
+    fn disconnect(&self) -> Result<DBResult, DBError> {
+        Err(DBError::DBDisconnectError { msg: "Unknown Err" })
+    }
+
+    fn put<T: RLPSerialize>(&self, key: &String, value: &T) -> Result<DBResult, DBError> {
+        Err(DBError::DBUpdateError { msg: "Unknown Err" })
+    }
+    fn get<T: RLPSerialize>(&self, key: &String) -> Result<T, DBError> {
+        Err(DBError::DBFetchError { msg: "Unknown Err" })
+    }
+
+    fn show_status(&self) -> Result<DBStatus, DBError> {
+        Err(DBError::DBStatusError { msg: "Unknown Err" })
+    }
+}
+
+#[cfg(not(mock))]
 impl DBManagerOP for DBManager {
     fn connect(&self,config: & DBConfig) -> Result<(&'static DBContext, DBResult), DBError> {
         Err(DBError::DBConnectError { msg: "Unknown Err" })
