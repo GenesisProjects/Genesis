48da4365e949fa616dc1182c6783bd08909bcbcb
Edward Tan <etan@fulgent.com>
2018-03-30 18:06:11 -0700

    Updated

--- GenesisRust/src/main.rs
+++ GenesisRust/src/main.rs
@@ -2,7 +2,7 @@ extern crate common;
 extern crate gen_core;
 extern crate rlp;
 
-use self::rlp::encoder::Encoder;
+use self::rlp::encoder::{ Encoder, SHARED_ENCODER };
 use self::rlp::decoder::Decoder;
 use self::rlp::types::RLP;
 use self::rlp::RLPSerialize;
@@ -10,10 +10,10 @@ use self::rlp::RLPSerialize;
 use self::gen_core::mpt::node::*;
 
 fn main() {
-    let test = TrieNode::LeafNode { encoded_path: vec![65u8, 65u8], value: "test".to_string() };
+    let test = TrieNode::ExtensionNode::<String> { encoded_path: vec![05u8, 65u8], key: [0u8; 32] };
     match test.serialize() {
         Ok(r) => {
-            print!("{:?}\n", r);
+            print!("{:?}\n", SHARED_ENCODER.lock().unwrap().encode(&r));
             let a = TrieNode::<String>::deserialize(&r);
             match a {
                 Ok(r) => { print!("{:?}\n", r); },
