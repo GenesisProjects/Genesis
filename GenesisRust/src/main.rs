24be19c7b69637de254d7cf220e65a91f7bc3a32
edwardtanshuo <tanshuo198956@gmail.com>
2018-03-31 16:21:48 -0800

    MPT node encoder

--- GenesisRust/src/main.rs
+++ GenesisRust/src/main.rs
@@ -14,17 +14,5 @@ use self::gen_core::mpt::node::*;
 use self::common::hash::SerializableAndSHA256Hashable;
 
 fn main() {
-    let mut i: u64 = 0;
-    let test = TrieNode::ExtensionNode::<String> { encoded_path: vec![00u8, 65u8], key: [0u8; 32] };
-    let rlp: RLP = match test.serialize() {
-        Ok(r) => r,
-        Err(_) => panic!("here")
-    };
 
-    let rlp_encoded = SHARED_ENCODER.lock().unwrap().encode(&rlp);
-    let rlp_decoded = Decoder::decode(&rlp_encoded);
-    print!("rlp{:?}\n", rlp);
-    print!("r{:?}\n", rlp_encoded);
-    print!("d{:?}\n", rlp_decoded);
-    SHARED_MANAGER.lock().unwrap().put(&test);
 }
