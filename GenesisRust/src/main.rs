c4b4e1bf4fd4cf803e27dcd96460c16194304a9f
Edward Tan <etan@fulgent.com>
2018-04-03 17:27:11 -0700

    Minor update.

--- GenesisRust/src/main.rs
+++ GenesisRust/src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
+extern crate gen_core;
+extern crate rlp;
 
+use gen_core::mpt::trie::*;
+use rlp::RLPSerialize;
+
+fn main() {
+    let mut test = Trie::new([0u8; 32]);
+    test.update(&"123".as_bytes().to_vec(), &"test".to_string());
+    test.update(&"124".as_bytes().to_vec(), &"test".to_string());
+    test.update(&"125".as_bytes().to_vec(), &"test".to_string());
+    let test_v = test.get(&"123".as_bytes().to_vec());
 }
