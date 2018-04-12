15d0631a99ec8150b6a3a4c8b8d0e30dab942e82
Edward Tan <etan@fulgent.com>
2018-04-12 15:39:10 -0700

    Add STUN module

--- network/src/nat/stun.rs
+++ network/src/nat/stun.rs
@@ -2,8 +2,29 @@
 /// reference: [[https://www.rfc-editor.org/rfc/rfc5389.txt]]
 
 use bytebuffer::*;
+use std::net::UdpSocket;
 
-/// STUN Diagram
+struct STUNManager {
+    buffer: ByteBuffer,
+}
+
+impl STUNManager {
+    pub fn new_with_size(size: usize) -> Self {
+        let mut buffer = ByteBuffer::new();
+        buffer.resize(size);
+        STUNManager { buffer: buffer }
+    }
+
+    pub fn new() -> Self {
+        let mut buffer = ByteBuffer::new();
+        buffer.resize(ENCODER_BUFFER_SIZE);
+        STUNManager { buffer: buffer }
+    }
+}
+
+
+
+/// STUN datagram
 /// 0                   1                   2                   3
 /// 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
@@ -15,3 +36,20 @@ use bytebuffer::*;
 /// |                     Transaction ID (96 bits)                  |
 /// |                                                               |
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+///
+
+enum STUNMessageType {
+
+}
+
+struct STUNHeader {
+
+}
+
+impl STUNHeader {
+    fn write_header(&self, manager: &mut STUNManager) {
+
+    }
+}
+
+
