de4d3e44614fc21543d2c14934ab8286a33ae03c
Edward Tan <etan@fulgent.com>
2018-04-16 18:12:00 -0700

    upnp

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,7 +1,9 @@
 /// [[https://tools.ietf.org/html/rfc5694]]
 pub mod peer;
 pub mod nat;
+pub mod ip_addr;
 
 #[macro_use]
 pub extern crate mio;
-extern crate bytebuffer;
+pub extern crate igd;
+pub extern crate bytebuffer;
