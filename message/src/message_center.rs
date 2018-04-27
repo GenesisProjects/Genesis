76c704f87a427582b7cd058a9e39302e7a4846dc
Edward Tan <etan@fulgent.com>
2018-04-27 15:23:26 -0700

    Updated message center

--- message/src/message_center.rs
+++ message/src/message_center.rs
@@ -6,10 +6,13 @@ use std::sync::Arc;
 use std::time::Duration;
 use std::net::SocketAddr;
 
+use gen_utils::config_parser::SETTINGS;
+
 pub struct MessageCenter {
     ip_addr: SocketAddr,
     event_loop: EventLoop,
-    listener: TcpListener
+    listener: TcpListener,
+    emitter: TcpStream
 }
 
 impl MessageCenter {
@@ -19,9 +22,14 @@ impl MessageCenter {
             Err(e) => panic!("Can not bind to the socket: {:?}", addr)
         };
 
+        let emitter = match TcpStream::connect(addr) {
+            Ok(r) => r,
+            Err(e) => panic!("Can not bind to the connect: {:?}", addr)
+        };
+
         let event_loop = EventLoop::new();
 
-        Some(MessageCenter { ip_addr: addr.to_owned(), event_loop: event_loop, listener: listener })
+        Some(MessageCenter { ip_addr: addr.to_owned(), event_loop: event_loop, listener: listener, emitter: emitter })
     }
 }
 
@@ -35,8 +43,16 @@ impl EventLoop {
         EventLoop { handle: None, poll_arc_pointer: None }
     }
 
+    fn register(&self, token: Token, listener: &TcpListener) {
+        // Start listening for incoming connections
+        if let Some(poll_pointer) = self.poll_arc_pointer.to_owned() {
+            poll_pointer.register(listener, token, Ready::readable(), PollOpt::edge()).unwrap();
+        }
+    }
+
     fn start(&mut self) {
-        let mut events: Events = Events::with_capacity(1024);
+        // Create events queue
+        let mut events: Events = Events::with_capacity(SETTINGS.read().unwrap().get_int("event_queue_size").unwrap() as usize);
         let poll = Poll::new().unwrap();
         // Retain a thread safe pointer into the current thread eventloop instant.
         let poll_arc_pointer = Arc::new(poll);
@@ -44,7 +60,6 @@ impl EventLoop {
         let thread_poll_arc_pointer = poll_arc_pointer.clone();
         self.poll_arc_pointer = Some(poll_arc_pointer.clone());
 
-        // Create storage for events
         let handle = thread::spawn(move|| {
             // Start main loop
             loop {
--- util/src/config_parser.rs
+++ util/src/config_parser.rs
@@ -16,5 +16,25 @@ lazy_static! {
 	    setting.merge(config::File::from(path_buff)).unwrap();
         setting
 	});
+
+	pub static ref BLOCK_SETTINGS: RwLock<config::Config> = RwLock::new({
+	    let mut setting = config::Config::default();
+	    let mut path_buff = env::current_dir().unwrap();
+	    path_buff.push("config");
+	    path_buff.push("block");
+	    path_buff.set_extension("json");
+	    setting.merge(config::File::from(path_buff)).unwrap();
+        setting
+	});
+
+	pub static ref NETWORK_SETTINGS: RwLock<config::Config> = RwLock::new({
+	    let mut setting = config::Config::default();
+	    let mut path_buff = env::current_dir().unwrap();
+	    path_buff.push("config");
+	    path_buff.push("network");
+	    path_buff.set_extension("json");
+	    setting.merge(config::File::from(path_buff)).unwrap();
+        setting
+	});
 }
 
