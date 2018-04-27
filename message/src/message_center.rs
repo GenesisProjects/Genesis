1ac2598f794a4f74ba523f548be49a0174ccea4c
Edward Tan <etan@fulgent.com>
2018-04-27 14:49:59 -0700

    Updated.

--- message/src/message_center.rs
+++ message/src/message_center.rs
@@ -2,6 +2,7 @@ use mio::*;
 use mio::net::{TcpListener, TcpStream};
 
 use std::thread;
+use std::sync::Arc;
 use std::time::Duration;
 use std::net::SocketAddr;
 
@@ -13,43 +14,48 @@ pub struct MessageCenter {
 
 impl MessageCenter {
     fn new(addr: &SocketAddr) -> Option<Self> {
-        let listener = TcpListener::bind(addr).unwrap();
-        let poll = Poll::new().unwrap();
-        let event_loop = EventLoop::new(poll);
-        panic!("")
-        /*if let Some(l) = listener {
-            Some(MessageCenter { ip_addr: addr.to_owned(), event_loop: event_loop, listener: l })
-        } else {
-            None
-        }*/
+        let listener = match TcpListener::bind(addr) {
+            Ok(r) => r,
+            Err(e) => panic!("Can not bind to the socket: {:?}", addr)
+        };
 
+        let event_loop = EventLoop::new();
+
+        Some(MessageCenter { ip_addr: addr.to_owned(), event_loop: event_loop, listener: listener })
     }
 }
 
 struct EventLoop {
-    poll: Poll,
-    handle: Option<thread::JoinHandle<()>>
+    handle: Option<thread::JoinHandle<()>>,
+    poll_arc_pointer: Option<Arc<Poll>>
 }
 
 impl EventLoop {
-    fn new(poll: Poll) -> Self {
-        EventLoop { poll: poll, handle: None }
+    fn new() -> Self {
+        EventLoop { handle: None, poll_arc_pointer: None }
     }
 
     fn start(&mut self) {
-        let mut events = Events::with_capacity(1024);
-        /*let handle = thread::spawn(|| {
-            // Create storage for events
+        let mut events: Events = Events::with_capacity(1024);
+        let poll = Poll::new().unwrap();
+        // Retain a thread safe pointer into the current thread eventloop instant.
+        let poll_arc_pointer = Arc::new(poll);
+        // Retain a thread safe pointer into the runloop thread
+        let thread_poll_arc_pointer = poll_arc_pointer.clone();
+        self.poll_arc_pointer = Some(poll_arc_pointer.clone());
 
+        // Create storage for events
+        let handle = thread::spawn(move|| {
             // Start main loop
             loop {
-                self.poll.poll(&mut events, None).unwrap();
-
+                // Poll the events
+                thread_poll_arc_pointer.poll(&mut events, None).unwrap();
+                // Dispatch events into handles
                 for event in events.iter() {
 
                 }
             }
         });
-        self.handle = Some(handle);*/
+        self.handle = Some(handle);
     }
 }
