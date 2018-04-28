fe7b2cc45e2d9e80f140cbdef7e7a831710dce6c
edwardtanshuo <tanshuo198956@gmail.com>
2018-04-28 12:55:38 -0800

    Added msg_center

--- message/src/message_center.rs
+++ message/src/message_center.rs
@@ -1,76 +1,104 @@
-use mio::*;
-use mio::net::{TcpListener, TcpStream};
+use std::sync::{Arc, Mutex, Condvar};
+use std::cell::RefCell;
+use std::collections::LinkedList;
 
-use std::thread;
-use std::sync::Arc;
-use std::time::Duration;
-use std::net::SocketAddr;
+const DEFAULT_MSG_QUEUE_SIZE: usize = 1024;
 
-use gen_utils::config_parser::SETTINGS;
-
-pub struct MessageCenter {
-    ip_addr: SocketAddr,
-    event_loop: EventLoop,
-    listener: TcpListener,
-    emitter: TcpStream
+#[derive(Copy, Clone)]
+enum SignalType {
+    Normal
 }
 
-impl MessageCenter {
-    fn new(addr: &SocketAddr) -> Option<Self> {
-        let listener = match TcpListener::bind(addr) {
-            Ok(r) => r,
-            Err(e) => panic!("Can not bind to the socket: {:?}", addr)
-        };
-
-        let emitter = match TcpStream::connect(addr) {
-            Ok(r) => r,
-            Err(e) => panic!("Can not bind to the connect: {:?}", addr)
-        };
-
-        let event_loop = EventLoop::new();
-
-        Some(MessageCenter { ip_addr: addr.to_owned(), event_loop: event_loop, listener: listener, emitter: emitter })
-    }
+struct MessageQueue {
+    queue: LinkedList<u16>,
+    limit: usize
 }
 
-struct EventLoop {
-    handle: Option<thread::JoinHandle<()>>,
-    poll_arc_pointer: Option<Arc<Poll>>
-}
-
-impl EventLoop {
-    fn new() -> Self {
-        EventLoop { handle: None, poll_arc_pointer: None }
-    }
-
-    fn register(&self, token: Token, listener: &TcpListener) {
-        // Start listening for incoming connections
-        if let Some(poll_pointer) = self.poll_arc_pointer.to_owned() {
-            poll_pointer.register(listener, token, Ready::readable(), PollOpt::edge()).unwrap();
+impl MessageQueue {
+    pub fn new(limit: usize) -> Self {
+        MessageQueue {
+            queue: LinkedList::<u16>::new(),
+            limit: limit
         }
     }
 
-    fn start(&mut self) {
-        // Create events queue
-        let mut events: Events = Events::with_capacity(SETTINGS.read().unwrap().get_int("event_queue_size").unwrap() as usize);
-        let poll = Poll::new().unwrap();
-        // Retain a thread safe pointer into the current thread eventloop instant.
-        let poll_arc_pointer = Arc::new(poll);
-        // Retain a thread safe pointer into the runloop thread
-        let thread_poll_arc_pointer = poll_arc_pointer.clone();
-        self.poll_arc_pointer = Some(poll_arc_pointer.clone());
+    pub fn enqueue_msg(&mut self, msg: u16) -> Result<usize, &'static str> {
+        let cur_size = self.queue.len();
+        if cur_size >= self.limit {
+            Err("The message queue is full.")
+        } else {
+            self.queue.push_back(msg);
+            Ok(cur_size + 1)
+        }
+    }
 
-        let handle = thread::spawn(move|| {
-            // Start main loop
-            loop {
-                // Poll the events
-                thread_poll_arc_pointer.poll(&mut events, None).unwrap();
-                // Dispatch events into handles
-                for event in events.iter() {
+    pub fn dequeue_msg(&mut self) -> Option<u16> {
+        self.queue.pop_front()
+    }
 
-                }
+    pub fn flush(&mut self) -> LinkedList<u16> {
+        let mut result = LinkedList::<u16>::new();
+        loop {
+            if self.queue.is_empty() { break; }
+            result.push_front(self.queue.pop_back().unwrap());
+        }
+        result
+    }
+
+    pub fn get_size(&self) -> usize {
+        self.queue.len()
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.queue.is_empty()
+    }
+}
+/// Message center is thread safe.
+pub struct MessageCenter {
+    cond_var_pair: Arc<(Mutex<SignalType>, Condvar)>,
+    msg_queue_pointer: RefCell<MessageQueue>
+}
+
+impl MessageCenter {
+    pub fn new() -> Self {
+        let msg_queue = MessageQueue::new(DEFAULT_MSG_QUEUE_SIZE);
+        let cond_var_pair = (Mutex::<SignalType>::new(SignalType::Normal), Condvar::new());
+        let msg_queue_arc_pointer = RefCell::new(msg_queue);
+        MessageCenter {
+            cond_var_pair: Arc::new(cond_var_pair),
+            msg_queue_pointer: msg_queue_arc_pointer
+        }
+    }
+
+    pub fn new_with_size(size: usize) -> Self {
+        let msg_queue = MessageQueue::new(size);
+        let cond_var_pair = (Mutex::<SignalType>::new(SignalType::Normal), Condvar::new());
+        let msg_queue_arc_pointer = RefCell::new(msg_queue);
+        MessageCenter {
+            cond_var_pair: Arc::new(cond_var_pair),
+            msg_queue_pointer: msg_queue_arc_pointer
+        }
+    }
+
+    pub fn send_msg(&mut self, msg: u16) {
+        let (ref mutex_lock, ref cond_var) = *self.cond_var_pair;
+        mutex_lock.lock().unwrap();
+        self.msg_queue_pointer.borrow_mut().enqueue_msg(msg);
+        cond_var.notify_one();
+    }
+
+    pub fn fetch_msg(&mut self) -> u16 {
+        let (ref mutex_lock, ref cond_var) = *(self.cond_var_pair);
+        let mut sync_start = mutex_lock.lock().unwrap();
+        let mut result: u16;
+        // loop to avoid spurious wakeup
+        loop {
+            sync_start = cond_var.wait(sync_start).unwrap();
+            if !self.msg_queue_pointer.borrow().is_empty() {
+                result = self.msg_queue_pointer.borrow_mut().dequeue_msg().unwrap();
+                break;
             }
-        });
-        self.handle = Some(handle);
+        }
+        result
     }
 }
