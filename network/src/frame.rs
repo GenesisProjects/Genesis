24b6ab57b831f3f7eea3b0f075387947cbb91c02
edwardtanshuo <tanshuo198956@gmail.com>
2018-05-01 22:57:36 -0800

    Frame buffer

--- network/src/frame.rs
+++ network/src/frame.rs
@@ -1,4 +1,6 @@
 use common::key::PublicKey;
+use bytebuffer::ByteBuffer;
+use std::io::{Error, ErrorKind};
 
 /// Frame Struct
 /// 0                   1                   2                   3
@@ -23,6 +25,9 @@ use common::key::PublicKey;
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
 /// |1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1|
 /// +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+const STUFFING_BYTES: u32 = 0xffffffff;
+const WINDOW_SIZE: usize = 1024 * 1024 * 16;
 
 enum FrameType {
     Request,
@@ -61,4 +66,64 @@ pub struct Frame {
     pub_key: PublicKey,
     role: Role,
     seq: SEQ,
+}
+
+struct FrameReader {
+    cache: [u8; WINDOW_SIZE],
+    r_pos: usize,
+    w_pos: usize
+}
+
+impl FrameReader {
+    pub fn read_frame(&mut self, buffer: &mut ByteBuffer) -> Result<Option<Frame>, Error> {
+        if !stick_mode {
+            self.flush();
+        }
+        let len = self.data_len();
+        unimplemented!()
+    }
+
+    fn read_cache(&mut self, size: usize) {
+        unimplemented!()
+    }
+
+    fn write_cache(&mut self, buffer: &[u8]) -> Result<usize, Error> {
+        let len = self.data_len();
+        if buffer.len() + len > WINDOW_SIZE {
+            Err(Error::new(ErrorKind::InvalidData, "The frame data is overflow"))
+        } else {
+            let a = self.cache[self.w_pos .. WINDOW_SIZE].clone_from_slice(buffer[0 .. WINDOW_SIZE - self.w_pos + 1]);
+            unimplemented!()
+        }
+    }
+
+    fn data_len(&self) -> usize {
+        if self.w_pos >= self.r_pos {
+            self.w_pos - self.r_pos
+        } else {
+            self.w_pos + WINDOW_SIZE - self.r_pos
+        }
+    }
+
+    fn find_tail(&self) -> Option<usize> {
+        let len = self.data_len();
+
+        if len < 4 {
+            return None;
+        }
+        for i in self.r_pos .. len - 3 {
+            if self.cache[i] == 0xffu8
+                && self.cache[(i + 1) % WINDOW_SIZE] == 0xffu8
+                && self.cache[(i + 2) % WINDOW_SIZE] == 0xffu8
+                && self.cache[(i + 3) % WINDOW_SIZE] == 0xffu8 {
+                return Some(i + 4 - r_pos);
+            }
+        }
+        None
+    }
+
+    fn flush(&mut self) {
+        self.w_pos = 0;
+        self.r_pos = 0;
+    }
 }
