adb9e296a6a0f28bcc9ed96d67aa71449199283f
Edward Tan <etan@fulgent.com>
2018-05-31 17:58:24 -0700

    Bug fixed

--- GenesisRust/src/main.rs
+++ GenesisRust/src/main.rs
@@ -4,7 +4,8 @@ use gen_network::nat::*;
 use std::net::SocketAddr;
 
 fn main() {
-    let test = get_local_ip();
+    let test = get_gateway_ip();
+    println!("{:?}", test);
     let result = get_public_ip_addr(Protocol::UPNP, &(SocketAddr::new(test.unwrap(), 9999), 9999));
-    println!("{:?}", result)
+    println!("{:?}", result);
 }
