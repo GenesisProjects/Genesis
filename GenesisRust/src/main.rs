2171408ad12327dd7bceafe1e525740b532522e6
Edward Tan <etan@fulgent.com>
2018-05-31 17:03:44 -0700

    UPMP

--- GenesisRust/src/main.rs
+++ GenesisRust/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
+extern crate gen_network;
+use gen_network::utils::*;
+use gen_network::nat::*;
+use std::net::SocketAddr;
 
+fn main() {
+    let test = get_local_ip();
+    let result = get_public_ip_addr(Protocol::UPNP, &(SocketAddr::new(test.unwrap(), 9999), 9999));
+    println!("{:?}", result)
 }
--- network/src/utils/mod.rs
+++ network/src/utils/mod.rs
@@ -1,7 +1,8 @@
 use std::process::Command;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
-use regex::Regex;
+use regex::{Regex, Captures};
 
+pub fn get_local_ip() -> Option<IpAddr> { get() }
 
 #[cfg(target_os = "linux")]
 fn get() -> Option<IpAddr> {
@@ -13,47 +14,37 @@ fn get() -> Option<IpAddr> {
 
     let re = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
     for cap in re.captures_iter(&stdout) {
-        if let Some(host) = cap.at(2) {
-            if host != "127.0.0.1" {
-                if let Ok(addr) = host.parse::<Ipv4Addr>() {
-                    return Some(IpAddr::V4(addr))
-                }
-                if let Ok(addr) = host.parse::<Ipv6Addr>() {
-                    return Some(IpAddr::V6(addr))
-                }
+        let host = &cap[2];
+        if host != "127.0.0.1" {
+            if let Ok(addr) = host.parse::<Ipv4Addr>() {
+                return Some(IpAddr::V4(addr))
+            } else if let Ok(addr) = host.parse::<Ipv6Addr>() {
+                return Some(IpAddr::V6(addr))
             }
         }
     }
-
     None
 }
 
 // TODO: windows version regex
 #[cfg(target_os = "windows")]
 fn get() -> Option<IpAddr> {
-    let output = Command::new("ifconfig")
+    let output = Command::new("ipconfig")
         .output()
-        .expect("failed to execute `ifconfig`");
+        .expect("failed to execute `ipconfig`");
 
     let stdout = String::from_utf8(output.stdout).unwrap();
 
-    let re = Regex::new(r#"(?m)^.*inet (addr:)?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
+    let re = Regex::new(r#"(?m)^.*IPv4 Address.*?(([0-9]*\.){3}[0-9]*).*$"#).unwrap();
     for cap in re.captures_iter(&stdout) {
-        if let Some(host) = cap.at(2) {
-            if host != "127.0.0.1" {
-                if let Ok(addr) = host.parse::<Ipv4Addr>() {
-                    return Some(IpAddr::V4(addr))
-                }
-                if let Ok(addr) = host.parse::<Ipv6Addr>() {
-                    return Some(IpAddr::V6(addr))
-                }
+        let host = &cap[1];
+        if host != "127.0.0.1" {
+            if let Ok(addr) = host.parse::<Ipv4Addr>() {
+                return Some(IpAddr::V4(addr))
+            } else if let Ok(addr) = host.parse::<Ipv6Addr>() {
+                return Some(IpAddr::V6(addr))
             }
         }
     }
-
     None
-}
-
-pub fn get_local_ip() -> Option<IpAddr> {
-    get()
 }
