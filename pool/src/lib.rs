6704060423813210bb83f06174a2b36f8bb42bbf
Edward Tan <etan@fulgent.com>
2018-08-13 18:25:42 -0700

    Observe trait accepts channel name as a subscribe params

--- pool/src/lib.rs
+++ pool/src/lib.rs
@@ -1,13 +1,15 @@
 pub extern crate common;
+pub extern crate gen_message;
 pub extern crate slab;
 
 use ::common::observe::*;
 use ::slab::Slab;
+use gen_message::*;
 
 pub struct Pool<T> {
     name: String,
     channels: Vec<String>,
-    slab: Slab<T>
+    slab: Slab<T>,
 }
 
 impl<T> Pool<T> {
@@ -25,5 +27,18 @@ impl<T> Pool<T> {
             .find(|r| r.1.to_owned() == name.to_owned())
             .unwrap()
             .0
+    }
+
+    pub fn notify_new_tx_recieved(&self) {
+        self.channels
+            .iter()
+            .map(|ch| {
+                MESSAGE_CENTER.lock()
+                    .unwrap()
+                    .send(
+                        ch.to_string(),
+                        Message::new(0, "new_tx".to_string()),
+                    );
+            });
     }
 }
