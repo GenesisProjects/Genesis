568280b57e2406a973bcd1d83223f0dacdbe7c76
Edward Tan <etan@fulgent.com>
2018-08-23 10:50:17 -0700

    Update doc

--- network/src/eventloop/mod.rs
+++ network/src/eventloop/mod.rs
@@ -1,3 +1,10 @@
+//! [mio](https://carllerche.github.io/mio/mio/index.html) event loop
+//!
+//! Network module allow operation system select sockets those are ready for I/O.
+//! Each round of loop are called **tick**.
+//! Inside one tick, NetworkEventLoop will poll and refresh I/O events.
+//! Objects implemented `Evented` could be registered in the NetworkEventLoop and obtain an auto-increase.
+//!
 use common::thread::ThreadStatus;
 use mio::*;
 use mio::net::TcpListener;
@@ -69,6 +76,39 @@ impl<T> NetworkEventLoop<T> where T: Evented {
         }
     }
 
+    /// Register a `TCPListener` instance with `SERVER_TOKEN`.
+    /// `TCPListener` will bind to a port and listen to connection event.
+    /// New connection will accept if event with `SERVER_TOKEN` has been triggered.
+    ///
+    /// # Example
+    /// ```
+    /// // Init a listener
+    /// let server = TcpListener::bind(&"127.0.0.1:19090".into());
+    ///
+    /// // Register the listener with a eventloop
+    /// let mut eventloop: NetworkEventLoop<ObjectEvented> = NetworkEventLoop::new(1024);
+    /// eventloop.register_server(&server);
+    ///
+    /// // Wait for connection
+    /// for event in &(self.eventloop.events) {
+    ///     match event.token() {
+    ///         SERVER_TOKEN => {
+    ///             println!("server event {:?}", event.token());
+    ///             match self.listener.accept() {
+    ///                 Ok((socket, addr)) => {
+    ///                     println!("New socket has been accepted")
+    ///                 }
+    ///                 Err(e) => {
+    ///                     println!("Could not accept the new connection.")
+    ///                 }
+    ///             }
+    ///         }
+    ///         _ => {
+    ///             println!("Not a server event, ignore it.")
+    ///         }
+    ///     }
+    /// }
+    /// ```
     pub fn register_server(&self, listener: &TcpListener) {
         let new_token = SERVER_TOKEN;
         let _ = self.poll.register(listener, new_token, Ready::readable(), PollOpt::edge());
@@ -94,6 +134,36 @@ impl<T> NetworkEventLoop<T> where T: Evented {
     /// Fetch new I/O ready events from sockets registered in the eventloop.
     /// Return number of ready sockets
     /// ```
+    /// // Define an evented object.
+    /// struct ObjectEvented;
+    /// impl Evented for ObjectEvented {
+    ///     fn register(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> Result<()> {
+    ///         unimplemented!()
+    ///     }
+    ///
+    ///     fn reregister(&self, poll: &Poll, token: Token, interest: Ready, opts: PollOpt) -> Result<()> {
+    ///         unimplemented!()
+    ///     }
+    ///
+    ///     fn deregister(&self, poll: &Poll) -> Result<()> {
+    ///         unimplemented!()
+    ///     }
+    /// }
+    ///
+    /// // Register the evented object.
+    /// let object = ObjectEvented;
+    /// let mut eventloop: NetworkEventLoop<ObjectEvented> = NetworkEventLoop::new(1024);
+    /// let token = eventloop.register_peer(&object).unwrap();
+    ///
+    /// // Do some I/O to `object`
+    ///
+    /// // Event loop fetch events
+    /// loop {
+    ///     eventloop.next_tick();
+    ///     for event in eventloop.events {
+    ///         // Do something here
+    ///     }
+    /// }
     ///
     /// ```
     pub fn next_tick(&mut self) -> Result<usize> {
