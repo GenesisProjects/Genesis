f20422cf646ce5ce9b82223faa22d9115a51b2be
Edward Tan <etan@fulgent.com>
2018-09-24 14:37:08 -0700

    Update

--- common/src/thread.rs
+++ common/src/thread.rs
@@ -3,6 +3,7 @@
 
 use std::io::*;
 use std::thread;
+use std::sync::{Mutex, Arc, MutexGuard};
 use std::time::Duration;
 
 use observe::*;
@@ -22,71 +23,106 @@ pub enum ThreadStatus {
     Pause
 }
 
-/// Thread trait
-pub trait Thread {
-    /// Launch a run loop.
-    /// Will create a thread context (controller) instance.
-    /// The thread will subscribe to the channel `name`.
-    fn launch<T>(name: String) where T: Observe + Thread, Self: Sized {
-        // TODO: make stack size configuable
-        thread::Builder::new().stack_size(64 * 1024 * 1024).name(name.to_owned()).spawn(move || {
-            let mut context = T::new(name.to_owned());
+/// Context cross thread reference
+pub struct ContextRef<T>(Arc<Mutex<T>>);
 
-            match &mut context {
-                &mut Ok(ref mut context_ref) => {
-                    context_ref.subscribe(name.to_owned());
-                    context_ref.set_status(ThreadStatus::Running);
-                    loop {
-                        let ret = context_ref.run();
-                        if let Some(msg) = context_ref.receive_async() {
-                            let forward_msg = msg.clone();
-                            match msg.msg.as_ref() {
-                                // Handle thread controll message
-                                "start" => {
-                                    context_ref.set_status(ThreadStatus::Running);
-                                },
-                                "pause" => {
-                                    context_ref.set_status(ThreadStatus::Pause);
-                                },
-                                "stop" => {
-                                    context_ref.set_status(ThreadStatus::Stop);
-                                },
-                                // Handle other thread message
-                                _ => {
-                                    context_ref.msg_handler(forward_msg);
-                                },
-                            }
-                        }
-
-                        if !ret {
-                            break;
-                        }
-                        thread::sleep(Duration::from_millis(LOOP_PERIOD));
-                    }
-                },
-                &mut Err(ref e) => {
-                    println!("Error: {:?}", e);
-                }
-            }
-            let _ = context.and_then(|mut context| {
-                context.unsubscribe(name.to_owned());
-                Ok(context)
-            });
-        }).unwrap();
+impl<T> ContextRef<T> {
+    pub fn wrap(obj: T) -> Self {
+        ContextRef(Arc::new(Mutex::new(obj)))
     }
 
-    /// run loop
-    fn run(&mut self) -> bool;
+    pub fn acquire(&self) -> MutexGuard<T> {
+        self.0.lock().unwrap()
+    }
+}
 
-    /// handle message
-    fn msg_handler(&mut self, msg: Message);
+impl<T> Clone for ContextRef<T> {
+    fn clone(&self) -> ContextRef<T> {
+        ContextRef(self.0.clone())
+    }
+}
+
+unsafe impl<T> Send for ContextRef<T> {}
+unsafe impl<T> Sync for ContextRef<T> {}
+
+/// Thread context initializer trait
+pub trait ThreadContextInitializer {
+    fn init(name: String) -> Self;
+}
+
+/// Thread information trait
+pub trait ThreadInfo {
+    /// get status
+    fn status(&self) -> ThreadStatus;
 
     /// set status
     fn set_status(&mut self, status: ThreadStatus);
 
-    /// get status
-    fn get_status(&self) -> ThreadStatus;
+    /// stack size
+    fn name() -> String;
+}
 
-    /// init context instance
-    fn new(name: String) -> Result<Self> where Self: Sized;
+/// Thread executor trait
+pub trait ThreadExec {
+    /// Run loop
+    fn exec(&mut self) -> bool;
+}
+
+/// Thread service trait
+pub trait ThreadService<CONTEXT_TYPE> {
+    /// Launch a thread.
+    /// Return context reference
+    fn launch(name: String, stack_size: usize) -> ContextRef<CONTEXT_TYPE>;
+
+    /// Start the run loop
+    fn start(&mut self);
+
+    /// Pause the run loop
+    fn pause(&mut self);
+
+    /// Break the run loop
+    fn stop(&mut self);
+}
+
+impl<CONTEXT_TYPE> ThreadService<CONTEXT_TYPE> for CONTEXT_TYPE where CONTEXT_TYPE: ThreadContextInitializer + ThreadInfo + ThreadExec {
+    fn launch(name: String, stack_size: usize) -> ContextRef<CONTEXT_TYPE> {
+        let mut context = CONTEXT_TYPE::init(name.to_owned());
+        context.set_status(ThreadStatus::Pause);
+        let context_ref: ContextRef<CONTEXT_TYPE> = ContextRef::wrap(context);
+        let thread_reserved_context_ref = context_ref.clone();
+
+        // Spawn a thread to hold the run loop
+        thread::Builder::new().stack_size(stack_size).name(name.to_owned()).spawn(move || {
+            loop {
+                let context_guard = thread_reserved_context_ref.acquire();
+                match context_guard.status() {
+                    ThreadStatus::Running => {
+                        // exec run loop
+                        context_guard.exec();
+                    },
+                    ThreadStatus::Pause => {
+                        // do nothing here
+                    },
+                    ThreadStatus::Stop => {
+                        // break run loop
+                        break;
+                    }
+                }
+                thread::sleep(Duration::from_millis(LOOP_PERIOD));
+            }
+        });
+        context_ref
+    }
+
+    fn start(&mut self) {
+        self.set_status(ThreadStatus::Running);
+    }
+
+    fn pause(&mut self) {
+        self.set_status(ThreadStatus::Pause);
+    }
+
+    fn stop(&mut self) {
+        self.set_status(ThreadStatus::Stop);
+    }
 }
